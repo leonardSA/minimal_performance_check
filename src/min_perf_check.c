7aa9e3135a70cbeabbdad015992fcddcd74b1b86
leonardSA <leonard.stephenauguste@gmail.com>
2020-02-13 19:36:27 +0100

    min_perf_check: modify to measure time of a mock thread

--- src/min_perf_check.c
+++ src/min_perf_check.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <sys/time.h>
 
+void measure_time();
+
+void* thread_mock_function(void* vargp) { sleep(3); };
+
 int main(int argc, char** argv) {
+    measure_time();
+    return EXIT_SUCCESS;
+}
+
+void measure_time() {
+    pthread_t tid;
     struct timeval before, after, elapsed;
+
     gettimeofday(&before, NULL);
-    sleep(1);
+    
+    pthread_create(&tid, NULL, thread_mock_function, NULL);
+    pthread_join(tid, NULL);
+
     gettimeofday(&after, NULL);
     timersub(&after, &before, &elapsed);
     printf("Time elapsed: %ld.%06ld\n", 
             (long int)elapsed.tv_sec, 
             (long int)elapsed.tv_usec);
-    return EXIT_SUCCESS;
 }
