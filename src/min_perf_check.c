cc5f3166671eb4fe7c342ec6176fd21ae7664fcb
leonardSA <leonard.stephenauguste@gmail.com>
2020-02-13 19:18:58 +0100

    min_perf_check: add minimal version of how to measure time

--- src/min_perf_check.c
+++ src/min_perf_check.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 int main(int argc, char** argv) {
-    fprintf(stdout, "Hello world!");
+    struct timeval before, after, elapsed;
+    gettimeofday(&before, NULL);
+    sleep(1);
+    gettimeofday(&after, NULL);
+    timersub(&after, &before, &elapsed);
+    printf("Time elapsed: %ld.%06ld\n", 
+            (long int)elapsed.tv_sec, 
+            (long int)elapsed.tv_usec);
     return EXIT_SUCCESS;
 }
